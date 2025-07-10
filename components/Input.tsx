4c7d78ca29d22732e55e4dc03067bc79defda7dc
jo4086 <ghzktm5892@gmail.com>
2025-07-10 22:08:01 +0900

    [etc] 기타 등등 커밋
    [2025-07-10]
    [Type]
    
    -

---
+++ components/Input.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { StyleSheet } from "react-native";
+import { View, Text } from "./Themed";
+
+export default function Input({ path }: { path: string }) {
+  return (
+    <View>
+      <Text>input Text</Text>
+    </View>
+  );
+}
